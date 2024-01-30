559f8033a998b411281016d5dd4664651b13b7c9
Lauri <lauri.kivinen12@gmail.com>
2024-01-30 13:42:32 +0200

    v1.6

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -33,18 +33,23 @@ public class Calculator {
         double result = 0;
 
         medianList = s.getIntGrades();
-        Collections.sort(medianList);
-        // If the list is odd
-        if (medianList.size() % 2 == 1) {
-            int x = (medianList.size()/2);    // To get the median element of the list (when it's odd) we can divided it by 2 and assign it to int which rounds it downwards ex. 7/2 = 3.5 => 3, , which is median element of the list since list elements start form 0
-            result = medianList.get(x);
-        } 
-        // If the list is even
+        if (medianList.size() == 0) {
+            result = 0;
+        }
         else {
-            int medianElement = medianList.size()/2;
-            double y = medianList.get(medianElement - 1);
-            double z = medianList.get(medianElement);
-            result = (y + z)/2;
+            Collections.sort(medianList);
+            // If the list is odd
+            if (medianList.size() % 2 == 1) {
+                int x = (medianList.size()/2);    // To get the median element of the list (when it's odd) we can divided it by 2 and assign it to int which rounds it downwards ex. 7/2 = 3.5 => 3, , which is median element of the list since list elements start form 0
+                result = medianList.get(x);
+            } 
+            // If the list is even
+            else {
+                int medianElement = medianList.size()/2;
+                double y = medianList.get(medianElement - 1);
+                double z = medianList.get(medianElement);
+                result = (y + z)/2;
+            }
         }
         return result;
     }
